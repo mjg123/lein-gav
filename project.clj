65c49c1dc5a12b527d114cd252569de8e23f3f4a
Matthew Gilliard <matthew.gilliard@gmail.com>
2015-11-25 21:09:48 +0000

    1.0.0 release

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject lein-gav "0.1.0-SNAPSHOT"
+(defproject lein-gav "1.0.0"
   :description "Leiningen plugin for printing projects' maven coordinates"
   :url "http://github.com/mjg123/lein-gav"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :scm {:name "git"
+        :url "https://github.com/mjg123/lein-gav"})
